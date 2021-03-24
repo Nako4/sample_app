14b7635b085462a2f4d890269950b29d96d2db3d
Nako4 <papnoatelier@gmail.com>
2021-03-25 06:58:12 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:sessions, :passwords, :registrations]
+
+  namespace :api do
+    post '/login', to: 'session#log_in'
+    post '/logout', to: 'session#log_out'
+    post '/account', to: 'accounts#show'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
